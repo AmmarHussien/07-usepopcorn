e9b9bab3b6f8d57ddddc00c3e487aff81f229011
ammarhussien <ammar.hussienmo97@gmail.com>
2024-03-20 13:17:17 +0200

    star rating and start propject

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StartRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating fullColor="Blue" onSetRating={setMovieRating} />
+      <p>this movie was reted {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={10}
+      fullColor="red"
+      strokeColor="blue"
+      textColor="orange"
+      size={50}
+    />
+    <StarRating className="test" />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating defaultRating={3} />
+
+    <StarRating maxRating={20} defaultRating={3} />
+
+    <Test />
+  </React.StrictMode>
+);
